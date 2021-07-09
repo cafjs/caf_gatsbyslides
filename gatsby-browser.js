8230ffa5b5a03f4b4bf6dbf87f254c08e6a9f405
Antonio Lain <antlai@cafjs.com>
2021-07-08 20:27:55 -0700

    Add fragment with page number
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,18 @@
 import "./src/styles/theme.scss";
+
+import {navigate} from '@reach/router';
+
+export function onClientEntry() {
+    if ((typeof window !== "undefined") && window.location &&
+        window.location.href && window.location.hash) {
+        const pageNum = parseInt(window.location.hash.slice(1));
+        if (!isNaN(pageNum)) {
+            const newURL = window.location.origin +
+                  (window.location.pathname.endsWith('/') ?
+                   window.location.pathname + pageNum :
+                   window.location.pathname + '/' + pageNum);
+            navigate(newURL);
+        }
+    }
+
+};
