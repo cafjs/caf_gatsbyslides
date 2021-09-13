a14eed4682955ca13fe91dd7601fbe65c4a880b4
Antonio Lain <antlai@cafjs.com>
2021-09-13 14:32:30 -0700

    Adding simulated keys
    Signed-off-by: Antonio Lain <antlai@cafjs.com>

--- src/gatsby-theme-mdx-deck/navigate.js
+++ src/gatsby-theme-mdx-deck/navigate.js
@@ -1,21 +1,26 @@
 // utilities for navigation
 import { navigate } from '@reach/router';
 
+const postInfo = (oldPage, newPage, step, msg) => {
+    if (window.parent && (window.parent !== window.self)) {
+        // in iframe
+        window.parent.postMessage({old: oldPage, new: newPage, step}, '*');
+        console.log(`${msg} old ${oldPage} new ${newPage} step ${step}`);
+    }
+}
+
 const nextSlide = ({ slug, length, index, setState }) => {
     const n = index + 1;
     if (n >= length) return;
     navigate([slug, n].join('/'));
     setState({ step: 0 });
-    if (window.parent && (window.parent !== window.self)) {
-        // in iframe
-        window.parent.postMessage({old: index, new: n}, '*');
-        console.log('nextSlide');
-    }
+    postInfo(index, n, 0, 'nextSlide');
 };
 
 export const next = context => {
     const { steps, step, setState } = context;
     if (!steps || step >= steps) return nextSlide(context);
+    postInfo(context.index, context.index, step + 1, 'nextStep');
     setState({ step: step + 1 });
 };
 
@@ -25,16 +30,13 @@ const previousSlide = ({ slug, index, metadata, setState }) => {
     navigate([slug, n].join('/'));
     const { steps = 0 } = metadata[n] || {};
     setState({ step: steps });
-    if (window.parent && (window.parent !== window.self)) {
-        // in iframe
-        window.parent.postMessage({old: index, new: n}, '*');
-        console.log('prevSlide');
-    }
+    postInfo(index, n, steps, 'prevSlide');
 };
 
 export const previous = context => {
     const { steps, step, setState } = context;
     if (steps && step > 0) {
+        postInfo(context.index, context.index, step - 1, 'prevStep');
         return setState({ step: step - 1 });
     }
     previousSlide(context);
